6b8b0fa896912f24422cccf502ce7ea1f1bf9967
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-08 17:32:41 -0500

    Implement fetching of threads from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react';
-// import { Post } from './types';
-import RT, { Static } from 'runtypes';
+import * as RT from 'runtypes';
 
 const Post = RT.Record({
   id: RT.Number,
   threadId: RT.Number,
   userId: RT.Number,
   content: RT.String,
-  datePosted: RT.InstanceOf(Date),
+  datePosted: RT.String,
 });
-
 const Posts = RT.Array(Post);
 
-type Posts = Static<typeof Posts>;
+const Thread = RT.Record({
+  id: RT.Number,
+  userId: RT.Number,
+  dateCreated: RT.String,
+  title: RT.String,
+});
+const Threads = RT.Array(Thread);
 
 function App() {
-  const [posts, setPosts] = useState<Posts>([]);
+  const [posts, setPosts] = useState<RT.Static<typeof Posts>>([]);
+  const [threads, setThreads] = useState<RT.Static<typeof Threads>>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -27,9 +32,25 @@ function App() {
     fetchPosts();
   }, []);
 
+  useEffect(() => {
+    const fetchThreads = async () => {
+      const response = await fetch('http://localhost:3001/api/threads');
+      const fetchedThreads = Threads.check(await response.json());
+      setThreads(fetchedThreads);
+    };
+
+    fetchThreads();
+  }, []);
+
   return (
     <div className="App">
       <h1>Forum App</h1>
+      <h3>threads</h3>
+      <ul>
+        {threads.map((thread) => (
+          <li key={thread.id}>{thread.title}</li>
+        ))}
+      </ul>
       <h3>posts</h3>
       <ul>
         {posts.map((post) => (
