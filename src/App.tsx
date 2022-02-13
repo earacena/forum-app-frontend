dc47dbecc6a31d03615f88d0c0cf922bfeec2cc9
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-13 17:28:54 -0500

    Refactor main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import postService from './services/postService';
-import threadService from './services/threadService';
-import { Posts, Threads } from './types';
+import React from 'react';
+import Threads from './components/Thread/Threads';
 
 function App() {
-  const [posts, setPosts] = useState<Posts>([]);
-  const [threads, setThreads] = useState<Threads>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const fetchedPosts = await postService.getAll();
-      setPosts(fetchedPosts);
-    };
-
-    fetchPosts();
-  }, []);
-
-  useEffect(() => {
-    const fetchThreads = async () => {
-      const fetchedThreads = await threadService.getAll();
-      setThreads(fetchedThreads);
-    };
-
-    fetchThreads();
-  }, []);
-
   return (
     <div className="App">
       <h1>Forum App</h1>
-      <h3>threads</h3>
-      <ul>
-        {threads.map((thread) => (
-          <li key={thread.id}>{thread.title}</li>
-        ))}
-      </ul>
-      <h3>posts</h3>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.content}</li>
-        ))}
-      </ul>
+      <Threads />
     </div>
   );
 }
