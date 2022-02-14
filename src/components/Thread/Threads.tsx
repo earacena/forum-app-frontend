71bbeb2678cc8cd157c5a5e080a8e33eb061e293
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-13 21:34:33 -0500

    Refactor Threads as a styled component

--- src/components/Thread/Threads.tsx
+++ src/components/Thread/Threads.tsx
@@ -1,6 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import threadService from '../../services/threadService';
 import { Threads as ThreadsType } from '../../types';
+
+const ThreadsTitle = styled.h2`
+  display: flex;
+  justify-content: center;
+  size: 10px;
+`;
+
+const ThreadsWrapper = styled.div`
+  padding: 1em;
+  border-radius: 0.4em;
+  background: linear-gradient(white, lightgrey, lightgrey);
+  margin: auto;
+`;
+
+const ThreadWrapper = styled.li`
+  display: flex;
+  justify-content: center;
+  margin-left: 3em;
+  margin-right: 3em;
+  padding: 1em;
+  background: darkgrey;
+  border-radius: 0.3em;
+  color: black;
+  border: 2px lightgrey solid;
+  &:hover {
+    background: white;
+    border: 2px black solid;
+  }
+`;
 
 function Threads() {
   const [threads, setThreads] = useState<ThreadsType>([]);
@@ -15,14 +45,14 @@ function Threads() {
   }, []);
 
   return (
-    <div>
-      <h3>Threads</h3>
-      <ul>
-        {threads.map((thread) => (
-          <li key={thread.id}>{thread.title}</li>
-        ))}
-      </ul>
-    </div>
+    <ThreadsWrapper>
+      <ThreadsTitle>Threads</ThreadsTitle>
+      {threads.map((thread) => (
+        <ThreadWrapper key={thread.id}>
+          {thread.title}
+        </ThreadWrapper>
+      ))}
+    </ThreadsWrapper>
   );
 }
 
