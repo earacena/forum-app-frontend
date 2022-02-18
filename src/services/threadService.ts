b11d8abb0c8a5ec25179088f3f6c61a73a76e657
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-18 10:08:37 -0500

    Implement service function for creating threads

--- src/services/threadService.ts
+++ src/services/threadService.ts
@@ -1,6 +1,10 @@
-import { ThreadArray } from '../types';
+import { Thread, ThreadArray } from '../types';
 
 const baseUrl = 'http://localhost:3001/api/threads';
+
+interface CreateFields {
+  title: string;
+}
 
 const getAll = async () => {
   const response = await fetch(baseUrl);
@@ -8,6 +12,19 @@ const getAll = async () => {
   return fetchedThreads;
 };
 
+const create = async ({ title }: CreateFields) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: title,
+  });
+  const createdThread = Thread.check(await response.json());
+  return createdThread;
+};
+
 export default {
   getAll,
+  create,
 };
