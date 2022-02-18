3c1b8f54f6f10b1912eecb958456546ba9cfa4f8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-18 10:08:07 -0500

    Implement service function for creating posts

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,17 +1,33 @@
-import { PostArray } from '../types';
+import { Post, PostArray } from '../types';
 
 const baseUrl = 'http://localhost:3001/api/posts';
 
+interface CreateFields {
+  content: string;
+}
+
 const getAll = async () => {
-  try {
-    const response = await fetch(baseUrl);
-    const fetchedPosts = PostArray.check(await response.json());
-    return fetchedPosts;
-  } catch (error) {
-    throw new Error(`${error}`);
-  }
+  const response = await fetch(baseUrl);
+  const fetchedPosts = PostArray.check(await response.json());
+  return fetchedPosts;
+};
+
+const create = async ({ content }: CreateFields) => {
+  const response = await fetch(
+    baseUrl,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: content,
+    },
+  );
+  const createdThread = Post.check(await response.json());
+  return createdThread;
 };
 
 export default {
   getAll,
+  create,
 };
