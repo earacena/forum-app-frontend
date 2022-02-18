4c602d061e49dbfc550db80def8b1431fe137139
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-17 20:27:19 -0500

    Remove try-catch statement to preserve stack trace

--- src/services/threadService.ts
+++ src/services/threadService.ts
@@ -3,13 +3,9 @@ import { ThreadArray } from '../types';
 const baseUrl = 'http://localhost:3001/api/threads';
 
 const getAll = async () => {
-  try {
-    const response = await fetch(baseUrl);
-    const fetchedThreads = ThreadArray.check(await response.json());
-    return fetchedThreads;
-  } catch (error) {
-    throw new Error(`${error}`);
-  }
+  const response = await fetch(baseUrl);
+  const fetchedThreads = ThreadArray.check(await response.json());
+  return fetchedThreads;
 };
 
 export default {
