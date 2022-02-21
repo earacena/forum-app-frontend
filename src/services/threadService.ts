5e91260eb80906a047d569ed70cc4257d883b27d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-20 19:47:08 -0500

    Include token to thread creation service function

--- src/services/threadService.ts
+++ src/services/threadService.ts
@@ -3,6 +3,7 @@ import { Thread, ThreadArray } from '../types';
 const baseUrl = 'http://localhost:3001/api/threads';
 
 interface CreateFields {
+  token: string;
   title: string;
 }
 
@@ -16,25 +17,32 @@ const getAll = async () => {
   return fetchedThreads;
 };
 
-const create = async ({ title }: CreateFields) => {
+const create = async ({ token, title }: CreateFields) => {
+  if (!token) {
+    throw new Error('Cannot perform this action if not signed in');
+  }
+
   const response = await fetch(baseUrl, {
     method: 'POST',
     headers: {
+      Authorization: `bearer ${token}`,
       'Content-Type': 'application/json',
     },
-    body: title,
+    body: JSON.stringify({ title }),
   });
+
+  if (response.status === 400) {
+    throw new Error('Bad Request while sending POST request for thread');
+  }
+
   const createdThread = Thread.check(await response.json());
   return createdThread;
 };
 
 const remove = async ({ id }: RemoveFields) => {
-  await fetch(
-    `${baseUrl}/${id}`,
-    {
-      method: 'DELETE',
-    },
-  );
+  await fetch(`${baseUrl}/${id}`, {
+    method: 'DELETE',
+  });
 };
 
 export default {
