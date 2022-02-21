a89f578e1164dba061c02d06f15922f687d9c50a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-20 19:31:15 -0500

    Add log out button and hide form after logging in

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import { removeAuthenticatedUser } from './features/auth/authSlice';
 import LoginForm from './features/loginForm/LoginForm';
-import Notification from './features/notification/Notification';
+import Notification, { notify } from './features/notification/Notification';
 import ThreadForm from './features/thread/ThreadForm';
 import Threads from './features/thread/Threads';
+import { useAppDispatch, useAppSelector } from './hooks';
+
+const LogOutButton = styled.button`
+  cursor: pointer;
+  border-radius: 30px;
+  padding: 0.6em;
+  color: lightgrey;
+  background: black;
+  border: 1px black solid;
+`;
 
 function App() {
+  const dispatch = useAppDispatch();
+  const auth = useAppSelector((state) => state.auth);
+
+  const handleLogOut = () => {
+    dispatch(removeAuthenticatedUser());
+    notify('message', 'Successfully logged out. Goodbye!', 4);
+  };
+
+  const loggedInUser = () => (
+    <div>
+      {`${auth.name} logged in `}
+      <LogOutButton onClick={handleLogOut}>Log Out</LogOutButton>
+    </div>
+  );
+
   return (
     <div className="App">
       <h1>Forum App</h1>
+      {!auth.token && <LoginForm />}
+      {auth.token && loggedInUser()}
       <Notification />
-      <LoginForm />
       <ThreadForm />
       <Threads />
     </div>
