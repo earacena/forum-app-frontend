37ceed0d23a1db0b86faf43018c49caa1e074ae3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-22 17:47:31 -0500

    Update Thread component to use Posts component

--- src/features/thread/Thread.tsx
+++ src/features/thread/Thread.tsx
@@ -1,35 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Number as RtNumber } from 'runtypes';
+import { Number as RtNumber, Static } from 'runtypes';
+import { Thread as ThreadType } from '../../types';
 import { useAppSelector, useAppDispatch } from '../../hooks';
 import threadService from '../../services/threadService';
 import { setPosts } from '../post/postsSlice';
+import Posts from '../post/Posts';
 
 function Thread() {
   const dispatch = useAppDispatch();
+  const [thread, setThread] = useState<Static<typeof ThreadType>>();
   const posts = useAppSelector((state) => state.posts);
   const { id } = useParams();
 
   useEffect(() => {
+    const fetchThread = async () => {
+      try {
+        const fetchedThread = await threadService.getThread({
+          id: RtNumber.check(Number(id)),
+        });
+        setThread(fetchedThread);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     const fetchPosts = async () => {
       try {
-        const fetchedPosts = await threadService.getPostsOfThread(
-          { id: RtNumber.check(Number(id)) },
-        );
+        const fetchedPosts = await threadService.getPostsOfThread({
+          id: RtNumber.check(Number(id)),
+        });
         dispatch(setPosts(fetchedPosts));
       } catch (error) {
         console.log(error);
       }
     };
 
+    fetchThread();
     fetchPosts();
   }, [id, dispatch]);
 
   return (
     <div>
-      This will be a page for thread with id
-      {` ${id}`}
-      {posts.map((post) => <p key={post.id}>{`${post.content} * ${post.datePosted}`}</p>)}
+      <h3>{thread?.title}</h3>
+      <Posts posts={posts} authorId={thread?.userId} />
     </div>
   );
 }
