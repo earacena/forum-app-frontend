e8da180a4906155afd59c0251cf8d4c99f1d995a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-22 13:33:59 -0500

    Incorporate thread form and make threads clickable

--- src/features/thread/Threads.tsx
+++ src/features/thread/Threads.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import threadService from '../../services/threadService';
+import ThreadForm from './ThreadForm';
 import { setThreads } from './threadSlice';
 
 const ThreadsTitle = styled.h2`
@@ -17,6 +19,7 @@ const ThreadListWrapper = styled.div`
 `;
 
 const ThreadWrapper = styled.p`
+  cursor: pointer;
   text-align: left;
   margin-left: 3em;
   margin-right: 3em;
@@ -34,6 +37,7 @@ const ThreadWrapper = styled.p`
 function Threads() {
   const dispatch = useAppDispatch();
   const threads = useAppSelector((state) => state.threads);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchThreads = async () => {
@@ -43,15 +47,26 @@ function Threads() {
 
     fetchThreads();
   }, [dispatch]);
+
+  const openThread = (id: number) => {
+    navigate(`/thread/${id}`);
+  };
+
   return (
-    <ThreadListWrapper>
-      <ThreadsTitle>Threads</ThreadsTitle>
-      {threads.map((thread) => (
-        <ThreadWrapper key={thread.id}>
-          {thread.title}
-        </ThreadWrapper>
-      ))}
-    </ThreadListWrapper>
+    <div>
+      <ThreadForm />
+      <ThreadListWrapper>
+        <ThreadsTitle>Threads</ThreadsTitle>
+        {threads.map((thread) => (
+          <ThreadWrapper
+            onClick={() => openThread(thread.id)}
+            key={thread.id}
+          >
+            {thread.title}
+          </ThreadWrapper>
+        ))}
+      </ThreadListWrapper>
+    </div>
   );
 }
 
