fbd67a2785c65061d1f782aa8ae602e136abc269
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-22 17:43:32 -0500

    Add user type for validation

--- src/types.ts
+++ src/types.ts
@@ -50,3 +50,20 @@ export const TokenResponse = RtRecord({
   username: RtString,
   name: RtString,
 });
+
+export const User = RtRecord({
+  id: RtNumber,
+  name: RtString,
+  username: RtString,
+  passwordHash: RtString,
+  dateRegistered: RtUnion(
+    RtInstanceOf(Date),
+    RtString.withConstraint((x: string) => {
+      // Must be parsable into a Date object
+      if (!x || x === null || typeof x !== 'string' || Number.isNaN(Date.parse(x))) {
+        return false;
+      }
+      return true;
+    }),
+  ),
+});
