39c1e2f531d2bf6c08eeafd9d02f6a172b756dd8
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-22 17:44:58 -0500

    Add component for displaying individual posts

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Static } from 'runtypes';
+import styled from 'styled-components';
+import { Post as PostType, User } from '../../types';
+import userService from '../../services/userService';
 
-function Post() {
-  return <div>test</div>;
+const PostWrapper = styled.div`
+  border: 1px red solid;
+  border-radius: 8px;
+  padding: 1em;
+`;
+
+interface PostProps {
+  post: Static<typeof PostType>;
+  isAuthor: boolean;
+}
+
+function Post({ post, isAuthor }: PostProps) {
+  const [user, setUser] = useState<Static<typeof User>>();
+  useEffect(() => {
+    const fetchUser = async () => {
+      const fetchedUser = await userService.getUserById({ id: post.userId });
+      setUser(fetchedUser);
+    };
+
+    fetchUser();
+  }, [post.userId]);
+
+  return (
+    <PostWrapper>
+      {`${user?.username} ${isAuthor ? '[Author]' : ''} on ${new Date(post.datePosted).toDateString()}`}
+      <hr />
+      {post.content}
+    </PostWrapper>
+  );
 }
 
 export default Post;
