7b83d8fd2c3b134c97bec9ea5733f55ad4a4d36a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-24 18:47:02 -0500

    Refactor Thread to use styled components

--- src/features/thread/Thread.tsx
+++ src/features/thread/Thread.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Number as RtNumber, Static } from 'runtypes';
+import styled from 'styled-components';
 import { Thread as ThreadType } from '../../types';
 import { useAppSelector, useAppDispatch } from '../../hooks';
 import threadService from '../../services/threadService';
 import { setPosts } from '../post/postsSlice';
 import Posts from '../post/Posts';
 import PostForm from '../post/PostForm';
+
+const ThreadTitle = styled.h3`
+  text-align: center;
+`;
 
 function Thread() {
   const dispatch = useAppDispatch();
@@ -45,7 +50,7 @@ function Thread() {
 
   return (
     <div>
-      <h3>{thread?.title}</h3>
+      <ThreadTitle>{thread?.title}</ThreadTitle>
       <Posts posts={posts} authorId={thread?.userId} />
       <PostForm threadId={threadId} />
     </div>
