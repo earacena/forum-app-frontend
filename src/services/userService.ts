6cb47bede858097bd7fd31f0db0ec1a7c7b4f05a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-23 20:23:49 -0500

    Add service function for creating users

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,12 +6,31 @@ interface GetUserByIdFields {
   id: number;
 }
 
+interface CreateUserFields {
+  name: string;
+  username: string;
+  password: string;
+}
+
 const getUserById = async ({ id }: GetUserByIdFields) => {
   const response = await fetch(`${baseUrl}/${id}`);
   const user = User.check(await response.json());
   return user;
 };
 
+const create = async ({ name, username, password }: CreateUserFields) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, username, password }),
+  });
+  const user = User.check(await response.json());
+  return user;
+};
+
 export default {
   getUserById,
+  create,
 };
