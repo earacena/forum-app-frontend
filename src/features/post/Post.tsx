950df57227ed4079069ea85435b151100f2fd42b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-24 18:43:05 -0500

    Refactor Post component to use authorName field
    Instead of fetching the user of every post to retrieve their name and
    username field, simply use the refactored Post model that includes
    an 'authorName' field to avoid unnecessary fetching from backend.

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Static } from 'runtypes';
 import styled from 'styled-components';
-import { Post as PostType, User } from '../../types';
-import userService from '../../services/userService';
+import { Post as PostType } from '../../types';
 
 const PostWrapper = styled.div`
   border: 1px red solid;
   border-radius: 8px;
   padding: 1em;
+  margin: 0 auto;
+  width: 60vw;
 `;
 
 interface PostProps {
@@ -16,19 +17,11 @@ interface PostProps {
 }
 
 function Post({ post, isAuthor }: PostProps) {
-  const [user, setUser] = useState<Static<typeof User>>();
-  useEffect(() => {
-    const fetchUser = async () => {
-      const fetchedUser = await userService.getUserById({ id: post.userId });
-      setUser(fetchedUser);
-    };
-
-    fetchUser();
-  }, [post.userId]);
-
   return (
     <PostWrapper>
-      {`${user?.username} ${isAuthor ? '[Author]' : ''} on ${new Date(post.datePosted).toDateString()}`}
+      {`${post?.authorName} ${isAuthor ? '[Author]' : ''} on ${new Date(
+        post.datePosted,
+      ).toDateString()}`}
       <hr />
       {post.content}
     </PostWrapper>
