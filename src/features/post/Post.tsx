ad8e4dbacf1dafaaafd709260bb848d34da9ab50
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-25 19:01:50 -0500

    Add indicators for thread authors and post authors
    By adding visual indications of the thread author's posts and the
    logged in user's posts, it makes it easier to distinguish interactions
    between users.

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -3,23 +3,30 @@ import { Static } from 'runtypes';
 import styled from 'styled-components';
 import { Post as PostType } from '../../types';
 
-const PostWrapper = styled.div`
-  border: 1px red solid;
+interface PostWrapperProps {
+  readonly threadAuthor: boolean;
+  readonly author: boolean;
+}
+
+const PostWrapper = styled.div<PostWrapperProps>`
+  border: ${(props) => (props.threadAuthor ? '3px grey dotted' : '')};
   border-radius: 8px;
   padding: 1em;
   margin: 0 auto;
   width: 60vw;
+  background: ${(props) => (props.author ? 'papayawhip' : '')};
 `;
 
 interface PostProps {
   post: Static<typeof PostType>;
+  isThreadAuthor: boolean;
   isAuthor: boolean;
 }
 
-function Post({ post, isAuthor }: PostProps) {
+function Post({ post, isThreadAuthor, isAuthor }: PostProps) {
   return (
-    <PostWrapper>
-      {`${post?.authorName} ${isAuthor ? '[Author]' : ''} on ${new Date(
+    <PostWrapper threadAuthor={isThreadAuthor} author={isAuthor}>
+      {`${post?.authorName} ${isThreadAuthor ? '[Author]' : ''} on ${new Date(
         post.datePosted,
       ).toDateString()}`}
       <hr />
