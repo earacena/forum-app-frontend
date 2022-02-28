b93881bfd1980e8fc9af6d83fded0f416d1ac758
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-02-28 17:30:17 -0500

    Add topic type definitions

--- src/types.ts
+++ src/types.ts
@@ -69,3 +69,27 @@ export const User = RtRecord({
     }),
   ),
 });
+
+export const Topic = RtRecord({
+  id: RtNumber,
+  userId: RtNumber,
+  title: RtString,
+  description: RtString,
+  dateCreated: RtUnion(
+    RtInstanceOf(Date),
+    RtString.withConstraint((x: string) => {
+      // Must be parsable into  date string
+      if (!x
+        || x === null
+        || typeof x !== 'string'
+        || Number.isNaN(Date.parse(x))
+      ) {
+        return false;
+      }
+
+      return true;
+    }),
+  ),
+});
+
+export const TopicArray = RtArray(Topic);
