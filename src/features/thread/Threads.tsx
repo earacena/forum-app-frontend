a9760b7756f25718f5596e6dcabd315e6d7faae1
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-01 18:30:16 -0500

    Fix state bug when reloading and update style
    The state of 'topics.currentTopic' is set when clicking on a topic in
    the Topics component, reloading the page while looking the threads of
    a topic resets the state. Therefore, the fix checks if current topic
    url is loaded onto the state and retrieves it from backend if it is
    not.

--- src/features/thread/Threads.tsx
+++ src/features/thread/Threads.tsx
@@ -6,37 +6,48 @@ import { useAppDispatch, useAppSelector } from '../../hooks';
 import ThreadForm from './ThreadForm';
 import { setThreads } from './threadSlice';
 import topicService from '../../services/topicService';
+import { setCurrentTopic } from '../topic/topicSlice';
 
 const ThreadsTitle = styled.h2`
   text-align: center;
-  size: 10px;
+  margin-bottom: 0;
 `;
 
-const ThreadsDescription = styled.h3`
+const ThreadsDescription = styled.p`
+  margin-top: 0;
   text-align: center;
+  font-size: 15px;
+  color: #404040;
 `;
 
 const ThreadListWrapper = styled.div`
+  border: 1px black solid;
   padding: 1em;
-  border-radius: 0.4em;
-  background: linear-gradient(white, gray, gray);
-  margin: auto;
+  border-radius: 5px;
+  margin: 1em;
 `;
 
-const ThreadWrapper = styled.p`
+const ThreadWrapper = styled.div`
+  display: flex;
   cursor: pointer;
-  text-align: left;
+  justify-content: space-between;
   margin-left: 3em;
   margin-right: 3em;
-  padding: 1em;
-  background: lightgrey;
-  border-radius: 0.3em;
+  padding: 0.5em;
+  border-radius: 8px;
   color: black;
-  border: 1px lightgrey solid;
-  &:hover {
-    background: white;
-    border: 1px black solid;
-  }
+  border: 1px black solid;
+`;
+
+const ThreadTitle = styled.p`
+  margin: 0;
+  align-self: center;
+  font-size: 120%;
+`;
+
+const LeftVerticalLine = styled.span`
+  border-left: 1px solid black;
+  padding: 0.5em;
 `;
 
 function Threads() {
@@ -49,12 +60,25 @@ function Threads() {
 
   useEffect(() => {
     const fetchThreads = async () => {
-      const fetchedThreads = await topicService.getThreadsOfTopic({ id: topicId });
+      const fetchedThreads = await topicService.getThreadsOfTopic({
+        id: topicId,
+      });
       dispatch(setThreads(fetchedThreads));
     };
 
     fetchThreads();
   }, [dispatch, topicId]);
+
+  useEffect(() => {
+    const fetchTopic = async () => {
+      const fetchedTopic = await topicService.getTopicById({ id: topicId });
+      dispatch(setCurrentTopic(fetchedTopic));
+    };
+
+    if (!topic) {
+      fetchTopic();
+    }
+  }, [dispatch, topicId, topic]);
 
   const openThread = (threadId: number) => {
     navigate(`/thread/${threadId}`);
@@ -62,17 +86,16 @@ function Threads() {
 
   return (
     <div>
-      <ThreadsTitle>{topic?.title}</ThreadsTitle>
-      <ThreadsDescription>{topic?.description}</ThreadsDescription>
       <ThreadForm />
       <ThreadListWrapper>
+        <ThreadsTitle>{topic?.title}</ThreadsTitle>
+        <ThreadsDescription>{topic?.description}</ThreadsDescription>
         {threads.map((thread) => (
-          <ThreadWrapper
-            onClick={() => openThread(thread.id)}
-            key={thread.id}
-          >
-            {thread.title}
-            <span style={{ display: 'block', textAlign: 'right' }}>{new Date(thread.dateCreated).toDateString()}</span>
+          <ThreadWrapper onClick={() => openThread(thread.id)} key={thread.id}>
+            <ThreadTitle>{thread.title}</ThreadTitle>
+            <LeftVerticalLine>
+              {new Date(thread.dateCreated).toDateString()}
+            </LeftVerticalLine>
           </ThreadWrapper>
         ))}
       </ThreadListWrapper>
