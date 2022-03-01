e1ceeab674ac342a0f1102cf98d9a8d443626916
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-01 18:29:26 -0500

    Implement function for retrieving topic by id

--- src/services/topicService.ts
+++ src/services/topicService.ts
@@ -1,6 +1,10 @@
-import { ThreadArray, TopicArray } from '../types';
+import { ThreadArray, Topic, TopicArray } from '../types';
 
 const baseUrl = 'http://localhost:3001/api/topics';
+
+interface TopicIdFields {
+  id: number;
+}
 
 interface TopicThreadsFields {
   id: number;
@@ -10,6 +14,16 @@ const getAll = async () => {
   const response = await fetch(baseUrl);
   const fetchedTopics = TopicArray.check(await response.json());
   return fetchedTopics;
+};
+
+const getTopicById = async ({ id }: TopicIdFields) => {
+  if (Number.isNaN(id)) {
+    throw new Error('invalid topic id number');
+  }
+
+  const response = await fetch(`${baseUrl}/${id}`);
+  const topic = Topic.check(await response.json());
+  return topic;
 };
 
 const getThreadsOfTopic = async ({ id }: TopicThreadsFields) => {
@@ -23,5 +37,6 @@ const getThreadsOfTopic = async ({ id }: TopicThreadsFields) => {
 
 export default {
   getAll,
+  getTopicById,
   getThreadsOfTopic,
 };
