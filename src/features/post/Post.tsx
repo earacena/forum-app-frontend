44b4c974719d43be38242ac520b8886c1f1861d3
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-03 18:38:34 -0500

    Implement button for users to delete their posts

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useFieldArray } from 'react-hook-form';
 import { Static } from 'runtypes';
 import styled from 'styled-components';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import postService from '../../services/postService';
 import { Post as PostType } from '../../types';
+import { setPosts } from './posts.slice';
 
 interface PostWrapperProps {
   readonly threadAuthor: boolean;
@@ -17,6 +21,22 @@ const PostWrapper = styled.div<PostWrapperProps>`
   background: ${(props) => (props.author ? 'papayawhip' : '')};
 `;
 
+const DeleteButton = styled.button`
+  cursor: pointer;
+  border-radius: 15px;
+  padding: 0.3em;
+  margin: 0;
+  margin-left: 1em;
+  color: lightgrey;
+  background: black;
+  border: 1px black solid;
+
+  &:hover {
+    background: darkred;
+    color: pink;
+  }
+`;
+
 interface PostProps {
   post: Static<typeof PostType>;
   isThreadAuthor: boolean;
@@ -24,11 +44,25 @@ interface PostProps {
 }
 
 function Post({ post, isThreadAuthor, isAuthor }: PostProps) {
+  const posts = useAppSelector((state) => state.posts);
+  const dispatch = useAppDispatch();
+  const auth = useAppSelector((state) => state.auth);
+
+  const handleDelete = async () => {
+    try {
+      await postService.remove({ id: post.id, token: auth.token });
+      dispatch(setPosts(posts.filter((p) => p.id !== post.id)));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <PostWrapper threadAuthor={isThreadAuthor} author={isAuthor}>
       {`${post?.authorName} ${isThreadAuthor ? '[Author]' : ''} on ${new Date(
         post.datePosted,
       ).toDateString()}`}
+      {isAuthor && (<DeleteButton onClick={handleDelete}>Delete</DeleteButton>)}
       <hr />
       {post.content}
     </PostWrapper>
