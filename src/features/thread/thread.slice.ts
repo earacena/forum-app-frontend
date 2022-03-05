8886a9016ed03150aa5e91ad77cdd4d2205a5906
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-04 19:19:23 -0500

    Expand threads state management to current thread

--- src/features/thread/thread.slice.ts
+++ src/features/thread/thread.slice.ts
@@ -1,20 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { Threads } from '../../types';
+import { Static as RtStatic } from 'runtypes';
+import { Thread, ThreadArray, Threads } from '../../types';
 
-const initialState: Threads = [];
+interface ThreadsState {
+  allThreads: RtStatic<typeof ThreadArray>;
+  currentThread: RtStatic<typeof Thread> | undefined;
+}
+
+const initialState: ThreadsState = {
+  allThreads: [],
+  currentThread: undefined,
+};
 
 const threadsSlice = createSlice({
   name: 'threads',
   initialState,
   reducers: {
-    setThreads: (state, action) => action.payload,
-    resetThreads: () => initialState,
+    setThreads: (state, action) => ({ ...state, allThreads: action.payload }),
+    setCurrentThread: (state, action) => ({ ...state, currentThread: action.payload }),
+    resetThreads: (state) => ({ ...state, allThreads: initialState.allThreads }),
+    resetCurrentThread: (state) => ({ ...state, currentThread: initialState.currentThread }),
   },
 });
 
 export const {
   setThreads,
+  setCurrentThread,
   resetThreads,
+  resetCurrentThread,
 } = threadsSlice.actions;
 
 export default threadsSlice.reducer;
