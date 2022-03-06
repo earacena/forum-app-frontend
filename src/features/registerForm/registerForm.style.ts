090b80509f46fe4421eaf10cc2e8b5500ef0ab12
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-06 15:45:56 -0500

    Remove unused components

--- src/features/registerForm/registerForm.style.ts
+++ src/features/registerForm/registerForm.style.ts
@@ -17,47 +17,6 @@ export const RegisterFormWrapper = styled.form`
   box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.4);
 `;
 
-export const Input = styled.input`
-  padding: 1em;
-  margin-top: 0;
-  margin: 1em;
-  border: 1px black solid;
-  border-radius: 3px;
-`;
-
-interface ButtonProps {
-  readonly primary?: boolean;
-}
-
-export const Button = styled.button<ButtonProps>`
-  cursor: pointer;
-  background: ${(props) => (props.primary ? 'black' : 'white')};
-  color: ${(props) => (props.primary ? 'lightgrey' : 'black')};
-  border-radius: 30px;
-  font-size: 1em;
-  padding: 1em;
-  margin: 1em auto;
-  border: 1px lightgrey solid;
-  box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.2);
-  width: 50%;
-  &:hover {
-    box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.4);
-    background: ${(props) => (props.primary ? 'black' : 'lightgrey')};
-    color: ${(props) => (props.primary ? 'white' : 'black')};
-  }
-
-  &:active {
-    box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.3);
-    transform: translateY(2px);
-    color: ${(props) => (props.primary ? 'lightgrey' : 'black')};
-  }
-`;
-
-export const ErrorMessage = styled.span`
-  color: red;
-  padding-left: 1em
-`;
-
 export const Label = styled.label`
   font-size: 17px;
   margin: 1em;
