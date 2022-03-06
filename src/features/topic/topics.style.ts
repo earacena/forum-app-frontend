6a1330e14d52d14e080dcfd821e821ffb247aca2
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-06 12:18:30 -0500

    Modify Topics style

--- src/features/topic/topics.style.ts
+++ src/features/topic/topics.style.ts
@@ -6,17 +6,29 @@ export const TopicTitle = styled.span`
 
 export const TopicListWrapper = styled.ul`
   padding: 1em;
-  border: 1px black solid;
-  border-radius: 4px;
+  border: 1px lightgrey solid;
+  border-radius: 8px;
   list-style-type: none;
   margin: 1em;
+  box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.3);
 `;
 
 export const TopicListItem = styled.li`
   cursor: pointer;
   padding: 1em;
-  border: 1px black solid;
+  border: 1px lightgrey solid;
   border-radius: 8px;
+  box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.17);
+  
+  &:hover {
+    border: 1px lightgrey solid;
+    box-shadow: 0 3px 10px rgb(0, 0, 0, 0.3);
+  }
+  
+  &:active {
+    box-shadow: 0 3px 10px rgb(0, 0, 0, 0.2);
+    transform: translateY(1px);
+  }
 `;
 
 export const LeftVerticalLine = styled.span`
