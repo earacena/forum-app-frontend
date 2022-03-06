05d8e66f08678549555facb9caa6460bc8c3ef8d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-06 15:47:03 -0500

    Remove unused components

--- src/features/thread/threadForm.style.ts
+++ src/features/thread/threadForm.style.ts
@@ -4,10 +4,6 @@ interface VisibilityProps {
   readonly visible?: boolean;
 }
 
-interface ButtonProps {
-  readonly primary?: boolean;
-}
-
 export const ThreadFormTitle = styled.h2`
   font-size: 1.5em;
   text-align: center;
@@ -24,12 +20,6 @@ export const ThreadFormWrapper = styled.form<VisibilityProps>`
   box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.2);
 `;
 
-export const Input = styled.input`
-  padding: 1em;
-  margin: 1em;
-  margin-top: 0;
-`;
-
 export const TextArea = styled.textarea`
   padding: 1em;
   margin: 1em;
@@ -38,32 +28,10 @@ export const TextArea = styled.textarea`
   height: 300px;
 `;
 
-export const Button = styled.button<ButtonProps>`
-  cursor: pointer;
-  background: ${(props) => (props.primary ? 'black' : 'white')};
-  color: ${(props) => (props.primary ? 'white' : 'black')};
-  border-radius: 30px;
-  font-size: 1em;
-  padding: 1em;
-  margin: auto;
-  border: 2px solid black;
-  box-shadow: 1px 1px 4px 2px grey;
-
-  &:hover {
-    background: ${(props) => (props.primary ? 'darkgrey' : 'grey')};
-    color: ${(props) => (props.primary ? 'black' : 'white')};
-  }
-`;
-
 export const Label = styled.label`
   font-size: 17px;
   margin: 1em;
   margin-bottom: 0;
-`;
-
-export const ErrorMessage = styled.p`
-  color: red;
-  padding-left: 1em;
 `;
 
 export const CreateButton = styled.button<VisibilityProps>`
