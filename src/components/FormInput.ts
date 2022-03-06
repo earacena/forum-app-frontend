fe0c2251f102daa0713c8b4c3333f835721040a7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-06 15:30:40 -0500

    Separate reusable FormInput into its own file

---
+++ src/components/FormInput.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const FormInput = styled.input`
+  padding: 1em;
+  margin-top: 0;
+  margin: 1em;
+  border: 1px black solid;
+  border-radius: 3px;
+`;
+
+export default FormInput;
