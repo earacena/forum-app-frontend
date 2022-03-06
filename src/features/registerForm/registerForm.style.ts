9e8fb246894f8f4fcb1c64f17f9028077fecc44b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-06 11:56:07 -0500

    Implement effect for pressing button

--- src/features/registerForm/registerForm.style.ts
+++ src/features/registerForm/registerForm.style.ts
@@ -30,17 +30,24 @@ interface ButtonProps {
 export const Button = styled.button<ButtonProps>`
   cursor: pointer;
   background: ${(props) => (props.primary ? 'black' : 'white')};
-  color: ${(props) => (props.primary ? 'white' : 'black')};
+  color: ${(props) => (props.primary ? 'lightgrey' : 'black')};
   border-radius: 30px;
   font-size: 1em;
   padding: 1em;
   margin: auto;
-  border: 2px solid black;
-  box-shadow: 1px 1px 4px 2px grey;
+  border: 1px lightgrey solid;
+  box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.2);
   width: 50%;
   &:hover {
-    background: ${(props) => (props.primary ? 'darkgrey' : 'grey')};
-    color: ${(props) => (props.primary ? 'black' : 'white')};
+    box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.4);
+    background: ${(props) => (props.primary ? 'black' : 'lightgrey')};
+    color: ${(props) => (props.primary ? 'white' : 'black')};
+  }
+
+  &:active {
+    box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.3);
+    transform: translateY(2px);
+    color: ${(props) => (props.primary ? 'lightgrey' : 'black')};
   }
 `;
 
