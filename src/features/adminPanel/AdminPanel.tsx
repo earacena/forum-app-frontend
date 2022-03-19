85c3ec093f6ff60e0d62dd97dfb30ba913a18a90
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-03-19 17:55:29 -0400

    Prevent users from viewing admin panel using route

--- src/features/adminPanel/AdminPanel.tsx
+++ src/features/adminPanel/AdminPanel.tsx
@@ -1,27 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { Static as RtStatic } from 'runtypes';
+import { useAppSelector } from '../../hooks';
 import userService from '../../services/userService';
 import { UserArray } from '../../types';
 import { notify } from '../notification/Notification';
 
 function AdminPanel() {
+  const auth = useAppSelector((state) => state.auth);
   const [users, setUsers] = useState<RtStatic<typeof UserArray>>([]);
+
+  const isAuthorizedToAccess = auth.token && auth.role === 'admin';
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const fetchedUsers = await userService.getAll();
-
         setUsers(fetchedUsers);
-
         notify('message', 'Fetched all users.', 4);
       } catch {
         notify('error', 'Error fetching users.', 4);
       }
     };
+    if (isAuthorizedToAccess) {
+      fetchUsers();
+    }
+  }, [isAuthorizedToAccess]);
 
-    fetchUsers();
-  }, []);
+  if (!isAuthorizedToAccess) {
+    return <div>Not authorized to view this page.</div>;
+  }
 
   return (
     <div>
