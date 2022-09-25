79904b3539793e6aefb6ee54df4cd4aad5bed055
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-25 19:52:04 -0400

    Add ThemeSwitcher style

--- src/components/ThemeSwitcher.ts
+++ src/components/ThemeSwitcher.ts
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 
 const ThemeSwitcher = styled.button`
+  display: flex;
+  border: none;
+  padding: 10px;
+  font-size: 20px;
+  border-radius: 50%;
+  color: ${(props) => props.theme.button.fg};
+  background: black;
+
+  &:hover {
+    background: ${(props) => props.theme.button.fg};
+    color: ${(props) => props.theme.button.bg};
+  }
 `;
 
 export default ThemeSwitcher;
