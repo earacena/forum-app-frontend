b3acce9f0bbba58944bc6823ee1504b6a2365c58
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-25 20:34:27 -0400

    Center ThemeSwitcher icon in button

--- src/components/ThemeSwitcher.ts
+++ src/components/ThemeSwitcher.ts
@@ -2,12 +2,15 @@ import styled from 'styled-components';
 
 const ThemeSwitcher = styled.button`
   display: flex;
+  align-items: center;
+  justify-content: center;
   border: none;
   padding: 10px;
   font-size: 20px;
   border-radius: 50%;
   color: ${(props) => props.theme.button.fg};
   background: black;
+  cursor: pointer;
 
   &:hover {
     background: ${(props) => props.theme.button.fg};
