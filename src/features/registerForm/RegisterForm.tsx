81993c7e119dc15af4b8632622c926cdbb50965b
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-01 17:14:32 -0400

    Add container for inputs

--- src/features/registerForm/RegisterForm.tsx
+++ src/features/registerForm/RegisterForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import styled from 'styled-components';
 import { useAppDispatch } from '../../hooks';
 import {
   RegisterFormTitle,
@@ -13,6 +14,13 @@ import { setAuthenticatedUser } from '../auth/auth.slice';
 import {
   FormSubmitButton, FormInput, ErrorMessage, FormLabel,
 } from '../../components';
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  margin-bottom: 30px;
+`;
 
 type Inputs = {
   name: string;
@@ -63,54 +71,56 @@ function RegisterForm() {
   return (
     <RegisterFormWrapper onSubmit={handleSubmit(onSubmit)}>
       <RegisterFormTitle>Register</RegisterFormTitle>
-      <FormLabel htmlFor="name">Display Name</FormLabel>
-      {errors.name?.type === 'maxLength' && (
-        <ErrorMessage>Name is too long</ErrorMessage>
-      )}
-      {errors.name?.type === 'required' && (
-        <ErrorMessage>This field is required</ErrorMessage>
-      )}
-      <FormInput
-        id="name"
-        type="text"
-        placeholder="Name"
-        {...register('name', { required: true, maxLength: 50 })}
-      />
+      <InputWrapper>
+        <FormLabel htmlFor="name">Display Name</FormLabel>
+        {errors.name?.type === 'maxLength' && (
+          <ErrorMessage>Name is too long</ErrorMessage>
+        )}
+        {errors.name?.type === 'required' && (
+          <ErrorMessage>This field is required</ErrorMessage>
+        )}
+        <FormInput
+          id="name"
+          type="text"
+          placeholder="Name"
+          {...register('name', { required: true, maxLength: 50 })}
+        />
 
-      <FormLabel htmlFor="username">Username</FormLabel>
-      {errors.username?.type === 'minLength' && (
-        <ErrorMessage>Username is too short</ErrorMessage>
-      )}
-      {errors.username?.type === 'maxLength' && (
-        <ErrorMessage>Username is too long</ErrorMessage>
-      )}
-      {errors.username?.type === 'required' && (
-        <ErrorMessage>This field is required</ErrorMessage>
-      )}
-      <FormInput
-        id="username"
-        type="text"
-        placeholder="Username"
-        {...register('username', {
-          required: true,
-          minLength: 5,
-          maxLength: 20,
-        })}
-      />
+        <FormLabel htmlFor="username">Username</FormLabel>
+        {errors.username?.type === 'minLength' && (
+          <ErrorMessage>Username is too short</ErrorMessage>
+        )}
+        {errors.username?.type === 'maxLength' && (
+          <ErrorMessage>Username is too long</ErrorMessage>
+        )}
+        {errors.username?.type === 'required' && (
+          <ErrorMessage>This field is required</ErrorMessage>
+        )}
+        <FormInput
+          id="username"
+          type="text"
+          placeholder="Username"
+          {...register('username', {
+            required: true,
+            minLength: 5,
+            maxLength: 20,
+          })}
+        />
 
-      <FormLabel htmlFor="password">Password</FormLabel>
-      {errors.password?.type === 'minLength' && (
-        <ErrorMessage>Password is too short</ErrorMessage>
-      )}
-      {errors.password?.type === 'required' && (
-        <ErrorMessage>This field is required</ErrorMessage>
-      )}
-      <FormInput
-        id="password"
-        type="password"
-        placeholder="Password"
-        {...register('password', { required: true, minLength: 8 })}
-      />
+        <FormLabel htmlFor="password">Password</FormLabel>
+        {errors.password?.type === 'minLength' && (
+          <ErrorMessage>Password is too short</ErrorMessage>
+        )}
+        {errors.password?.type === 'required' && (
+          <ErrorMessage>This field is required</ErrorMessage>
+        )}
+        <FormInput
+          id="password"
+          type="password"
+          placeholder="Password"
+          {...register('password', { required: true, minLength: 8 })}
+        />
+      </InputWrapper>
 
       <FormSubmitButton primary type="submit">Register User</FormSubmitButton>
     </RegisterFormWrapper>
