a783916574a6a5d4a7d7b150d0a65cc8ff9e9a2a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-09-30 20:49:02 -0400

    Modify FormSubmitButton to use colorAccent style

--- src/components/FormSubmitButton.ts
+++ src/components/FormSubmitButton.ts
@@ -6,15 +6,13 @@ interface ButtonProps {
 
 const FormSubmitButton = styled.button<ButtonProps>`
   cursor: pointer;
-  background: ${(props) => (props.primary ? 'black' : 'white')};
-  color: ${(props) => (props.primary ? 'lightgrey' : 'black')};
+  background: ${(props) => (props.theme.colorAccent)};
+  color: ${(props) => (props.theme.bg)};
   border-radius: 30px;
   font-size: 1em;
   padding: 1em;
-  margin: 1em auto;
   border: 1px black solid;
   box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.2);
-  width: 50%;
   &:hover {
     box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.4);
     background: ${(props) => (props.primary ? 'black' : 'lightgrey')};
