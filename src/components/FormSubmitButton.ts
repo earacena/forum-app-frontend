6e42fe37669faaafacbb169882c51c51d54a9276
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-01 17:12:38 -0400

    Modify button width and label color

--- src/components/FormSubmitButton.ts
+++ src/components/FormSubmitButton.ts
@@ -7,12 +7,13 @@ interface ButtonProps {
 const FormSubmitButton = styled.button<ButtonProps>`
   cursor: pointer;
   background: ${(props) => (props.theme.colorAccent)};
-  color: ${(props) => (props.theme.bg)};
+  color: ${(props) => (props.theme.form.inputBg)};
   border-radius: 30px;
   font-size: 1em;
   padding: 1em;
   border: 1px black solid;
   box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.2);
+  width: 70%;
   &:hover {
     box-shadow: 0px 3px 10px rgb(0, 0, 0, 0.4);
     background: ${(props) => (props.primary ? 'black' : 'lightgrey')};
