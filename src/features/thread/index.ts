31dead37d41058bd1f7c0df2bf5a2ca84157e92c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-04 21:26:41 -0400

    Re-export Threads feature components and actions

---
+++ src/features/thread/index.ts
@@ -0,0 +1,10 @@
+export { default as Thread } from './Thread';
+export { default as ThreadForm } from './ThreadForm';
+export { default as Threads } from './Threads';
+export {
+  default as ThreadsReducer,
+  setThreads,
+  setCurrentThread,
+  resetThreads,
+  resetCurrentThread,
+} from './thread.slice';
