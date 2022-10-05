595526e12fd9cc0a0fff47d11c9cd17298feedf7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-04 21:27:47 -0400

    Re-export Topics feature components and actions

---
+++ src/features/topic/index.ts
@@ -0,0 +1,9 @@
+export {
+  default as TopicsReducer,
+  setTopics,
+  setCurrentTopic,
+  resetTopics,
+  resetCurrentTopic,
+} from './topic.slice';
+export { default as Topics } from './Topics';
+export { default as TopicForm } from './TopicForm';
