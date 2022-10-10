0a89c8058d10825c60fb588ce4a26ab6dba0594c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-09 20:50:59 -0400

    Restructure Notification folder

---
+++ src/features/Notification/index.ts
@@ -0,0 +1,2 @@
+export { default as Notification, notify } from './Notification';
+export { setNotificationMessage, removeNotificationMessage } from './stores/notification.slice';
--- src/features/notification/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Notification, notify } from './Notification';
-export { setNotificationMessage, removeNotificationMessage } from './notification.slice';
