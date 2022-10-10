708d7e6f29b786c46b32179e4349e167a4ff69c9
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-09 21:35:42 -0400

    Update import paths

--- src/features/Threads/Thread.tsx
+++ src/features/Threads/Thread.tsx
@@ -6,7 +6,7 @@ import { BiArrowBack } from 'react-icons/bi';
 import { useAppSelector, useAppDispatch } from '../../hooks';
 import { Posts, PostForm, setPosts } from '../Posts';
 import { setCurrentThread } from './stores/thread.slice';
-import { setCurrentTopic } from '../topic';
+import { setCurrentTopic } from '../Topics';
 import threadService from '../../services/threadService';
 import topicService from '../../services/topicService';
 import { ThreadWrapper } from './styles/threads.style';
