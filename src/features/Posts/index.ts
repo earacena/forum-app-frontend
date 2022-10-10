7fc5ca9f4cb3721c77001255a4241fb3e14bba36
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-09 21:38:29 -0400

    Rename exported reducer

--- src/features/Posts/index.ts
+++ src/features/Posts/index.ts
@@ -2,4 +2,4 @@ export { default as Post } from './Post';
 export { default as PostEditForm } from './components/PostEditForm/PostEditForm';
 export { default as PostForm } from './components/PostForm/PostForm';
 export { default as Posts } from './Posts';
-export { default as PostsReducer, setPosts, resetPosts } from './stores/posts.slice';
+export { default as postsReducer, setPosts, resetPosts } from './stores/posts.slice';
