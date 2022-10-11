5b8ccbc465834570324e729c7b452577a298ddbb
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-10 22:17:15 -0400

    Increase bottom margin

--- src/components/FormLabel.ts
+++ src/components/FormLabel.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const FormLabel = styled.label`
   font-size: 17px;
   margin: 1em;
-  margin-bottom: 0;
+  margin-bottom: 5px;
   color: ${(props) => props.theme.fg}
 `;
 
