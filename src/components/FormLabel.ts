3e26836cdeef21bad4ba496193469ec482a333ca
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-13 15:59:08 -0400

    Modify margin to a explicitly defined value

--- src/components/FormLabel.ts
+++ src/components/FormLabel.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const FormLabel = styled.label`
   font-size: 17px;
-  margin: 1em;
+  margin: 10px;
   margin-bottom: 5px;
   color: ${(props) => props.theme.fg}
 `;
