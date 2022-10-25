485b8df52652a6a796c8470284deda0f73ec6d68
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-10-25 16:36:48 -0400

    Remove unused imports

--- src/features/Posts/Posts.tsx
+++ src/features/Posts/Posts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAppSelector } from '../../hooks';
 import { Posts as PostsType } from '../../types';
 import Post from './Post';
