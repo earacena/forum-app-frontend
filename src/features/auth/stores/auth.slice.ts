b050bfa848247c8dbf0c1c97aa3c4e56b930a097
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-11-14 15:32:53 -0500

    Add types to auth slice

--- src/features/auth/stores/auth.slice.ts
+++ src/features/auth/stores/auth.slice.ts
@@ -1,32 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  AuthenticatedUserPayload,
+  UserState,
+} from '../types/auth.types';
 
-const initialState: UserAuthentication = {
-  id: 0,
-  token: '',
-  username: '',
-  name: '',
-  role: '',
+const initialState: UserState = {
+  user: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setAuthenticatedUser: (state, action) => action.payload,
+    setAuthenticatedUser: (state: UserState, action: PayloadAction<AuthenticatedUserPayload>) => ({
+      ...state,
+      user: action.payload.user,
+    }),
     removeAuthenticatedUser: () => initialState,
   },
 });
-
-export interface UserAuthentication {
-  id: number;
-  token: string;
-  username: string;
-  name: string;
-  role: string;
-}
 
 export const {
   setAuthenticatedUser,
   removeAuthenticatedUser,
 } = authSlice.actions;
+
 export default authSlice.reducer;
