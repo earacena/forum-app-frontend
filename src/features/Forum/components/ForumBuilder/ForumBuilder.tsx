2b353333ea7a9b2173be169d9936a8b7153922e7
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-11-13 19:02:06 -0500

    Implement loading spinner for submit button

--- src/features/Forum/components/ForumBuilder/ForumBuilder.tsx
+++ src/features/Forum/components/ForumBuilder/ForumBuilder.tsx
@@ -1,8 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { GrAdd } from 'react-icons/gr';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
+import { ThemeContext } from 'styled-components';
 import { useAppSelector } from '../../../../hooks';
-import { FormInput, FormLabel, FormSubmitButton } from '../../../../components';
+import {
+  FormInput,
+  FormLabel,
+  FormSubmitButton,
+  Spin,
+} from '../../../../components';
 import { notify } from '../../../Notification';
 import forumService from '../../../../services/forumService';
 import type { TopicItem } from '../../../../types';
@@ -17,15 +24,25 @@ import {
 } from './styles/forumBuilder.styles';
 
 function ForumBuilderForm() {
-  const auth = useAppSelector((state) => state.auth);
+  const theme = useContext(ThemeContext);
   const navigate = useNavigate();
+
+  const auth = useAppSelector((state) => state.auth);
   const [forumTitle, setForumTitle] = useState<string>('');
   const [topicFields, setTopicFields] = useState<TopicItem[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
   const isUserLoggedIn = auth.token !== '';
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Ignore empty submissions
+    if (forumTitle === '' || topicFields.length === 0) {
+      return;
+    }
+
     try {
-      event.preventDefault();
+      setIsSubmitting(true);
 
       const newForum = await forumService.create({
         token: auth.token,
@@ -36,6 +53,7 @@ function ForumBuilderForm() {
       navigate(`/forum/${newForum.id}`);
     } catch (error: unknown) {
       notify('error', 'Error processing given forum details.', 4);
+      setIsSubmitting(false);
     }
   };
 
@@ -106,7 +124,12 @@ function ForumBuilderForm() {
 
       <AddTopicButton type="button" onClick={handleAddTopic}><GrAdd /></AddTopicButton>
 
-      <FormSubmitButton style={{ margin: '30px', marginLeft: 0 }}>Create</FormSubmitButton>
+      <FormSubmitButton
+        style={{ margin: '30px', marginLeft: 0 }}
+        disabled={isSubmitting}
+      >
+        {!isSubmitting ? 'Create' : <Spin><AiOutlineLoading3Quarters style={{ color: theme.bg, fontSize: '40px' }} /></Spin>}
+      </FormSubmitButton>
     </ForumBuilderWrapper>
   );
 }
